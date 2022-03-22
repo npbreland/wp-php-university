3c046e1500cfd5ca261f6a7cdcba816f832f66ae
Nick Breland <npbreland@tutanota.com>
2022-03-22 21:48:37 +0000

    More validation

--- wp-php-university.php
+++ wp-php-university.php
@@ -1,10 +1,12 @@
 <?php
+namespace NPBreland\WPUni;
+
+require_once 'vendor/npbreland/php-university/autoload.php';
 
 /**
  * Plugin Name: WP PHP University
  */
 
-namespace WPUni;
 
 // This plugin bundles ACF, but doesn't require it if it's already installed.
 if ( !class_exists('ACF') ) {
@@ -82,6 +84,47 @@ function acf_add_local_field_groups()
 				'type' => 'user',
                 'role' => [ 'instructor' ]
 			],
+			[
+				'key' => 'field_phpuni_class_days',
+				'label' => 'Days',
+				'name' => 'class_days',
+				'type' => 'select',
+                'ui' => 1,
+                'choices' => [ 
+                    '1' => 'Monday',
+                    '2' => 'Tuesday',
+                    '3' => 'Wednesday',
+                    '4' => 'Thursday',
+                    '5' => 'Friday',
+                    '6' => 'Saturday',
+                    '7' => 'Sunday',
+                ],
+                'multiple' => 1,
+			],
+			[
+				'key' => 'field_phpuni_class_start_time',
+				'label' => 'Start time',
+				'name' => 'class_start_time',
+				'type' => 'time_picker',
+			],
+			[
+				'key' => 'field_phpuni_class_end_time',
+				'label' => 'End time',
+				'name' => 'class_end_time',
+				'type' => 'time_picker',
+			],
+			[
+				'key' => 'field_phpuni_class_start_date',
+				'label' => 'Start date',
+				'name' => 'class_start_date',
+				'type' => 'date_picker',
+			],
+			[
+				'key' => 'field_phpuni_class_end_date',
+				'label' => 'End date',
+				'name' => 'class_end_date',
+				'type' => 'date_picker',
+			],
 		],
 		'location' => [
 			[
@@ -97,6 +140,119 @@ function acf_add_local_field_groups()
 
 add_action( 'init', __NAMESPACE__ . '\\setup_post_types' );
 add_action( 'acf/init', __NAMESPACE__ . '\\acf_add_local_field_groups');
+
+add_filter( 'acf/validate_value/name=course_code', __NAMESPACE__ . '\\validate_course_code', 10, 4 );
+function validate_course_code( $valid, $value, $field, $input_name )
+{
+    // Bail early if value is already invalid.
+    if( $valid !== true ) {
+        return $valid;
+    }
+
+    $ID = get_the_ID();
+
+    $course_with_this_code = get_course_by_code( $value );
+
+    if ( $course_with_this_code && $course_with_this_code != $ID ) {
+        $err_msg = <<<MSG
+Another course has the course code $value. Please enter a unique course code.
+MSG;
+        return $err_msg;
+    }
+
+    return $valid;
+}
+
+
+add_action( 'acf/validate_save_post', __NAMESPACE__ . '\\validate_save');
+function validate_save()
+{
+    $start_time = $_POST['acf']['field_phpuni_class_start_time'];
+    $end_time = $_POST['acf']['field_phpuni_class_end_time'];
+
+    if ( isset($start_time) && $start_time >= $end_time ) {
+        $err_msg = "The end time must be greater than start time.";
+        \acf_add_validation_error( 'acf[field_phpuni_class_end_time]', $err_msg );
+    }
+
+    $start_date = new \DateTime($_POST['acf']['field_phpuni_class_start_date']);
+    $end_date = new \DateTime($_POST['acf']['field_phpuni_class_end_date']);
+
+    if ( $start_date >= $end_date ) {
+        $err_msg = "The end date must be greater than start date.";
+        \acf_add_validation_error( 'acf[field_phpuni_class_end_date]', $err_msg );
+    }
+
+    $days = $_POST['acf']['field_phpuni_class_days'];
+
+    if ( !in_array( $start_date->format('N'), $days ) ) {
+        $err_msg = "The start date must be one of the selected days.";
+        \acf_add_validation_error( 'acf[field_phpuni_class_start_date]', $err_msg );
+    }
+
+    if ( !in_array( $end_date->format('N'), $days ) ) {
+        $err_msg = "The end date must be one of the selected days.";
+        \acf_add_validation_error( 'acf[field_phpuni_class_end_date]', $err_msg );
+    }
+
+    $instructor_id = $_POST['acf']['field_phpuni_class_instructor'];
+    $all_classes_by_them = get_classes_by_instructor( $instructor_id );
+
+    $this_class_ID = get_the_ID();
+
+    $other_classes = array_filter($all_classes_by_them, function ($class_ID) use ($this_class_ID) {
+        return $class_ID != $this_class_ID; 
+    });
+
+}
+
+function get_course_by_code( $code )
+{
+    $args = [
+        'posts_per_page' => -1,
+        'post_type' => 'course',
+        'meta_key' => 'course_code',
+        'meta_value' => $code,
+    ];
+
+    $ID = false;
+
+    $the_query = new \WP_Query( $args );
+    if ( $the_query->have_posts() ) {
+        while ( $the_query->have_posts() ) {
+            $the_query->the_post();
+            $ID = get_the_ID();
+        }
+    }
+
+    wp_reset_query();
+
+    return $ID;
+}
+
+function get_classes_by_instructor( $user_id )
+{
+    $args = [
+        'posts_per_page' => -1,
+        'post_type' => 'class',
+        'meta_key' => 'class_instructor',
+        'meta_value' => $user_id,
+    ];
+
+    $IDs = [];
+
+    $the_query = new \WP_Query( $args );
+    if ( $the_query->have_posts() ) {
+        while ( $the_query->have_posts() ) {
+            $the_query->the_post();
+            $IDs[] = get_the_ID();
+        }
+    }
+
+    wp_reset_query();
+
+    return $IDs;
+}
 
 function add_roles()
 {
